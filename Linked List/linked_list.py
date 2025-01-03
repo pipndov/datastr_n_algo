6e24f0975dcf2329b0cf895ac0622a0e87f4c701
pipndov <pipndov@gmail.com>
2025-01-02 20:21:49 -0500

    bst practice

--- Linked List/linked_list.py
+++ Linked List/linked_list.py
@@ -5,18 +5,18 @@ class Node:
 
 class LinkedList:
     def __init__(self, val):
-        new_node = Node(val)
-        self.head = new_node
-        self.tail = new_node
-        self.length = 1
+        new_node = Node(val)            # the value is inserted into the node class providing it with a next pointer
+        self.head = new_node            # initialize Linked List will have first Node be head
+        self.tail = new_node            # initialize Linked List will have first Node be tail
+        self.length = 1                 # track length 
 
     def insertNode(self,val):
-        new_node = Node(val)
-        if self.head == None:
+        new_node = Node(val)            
+        if self.head is None:
             self.head = new_node
             self.tail = new_node
             self.length = 1
-        self.tail.next = new_node
-        self.tail = new_node
+        self.tail.next = new_node       # have the tail point the new node
+        self.tail = new_node            # new node now becomes the tail
         self.length += 1
 
